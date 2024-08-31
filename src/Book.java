fa1720dc1129be4138cd04c963c327735e9f401a
pugeaale <pugeaale@gmail.com>
2024-08-31 11:56:12 +0200

    :sparkles: adding basic Book class

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+//Create a Book class with properties like title, author, and ISBN.
+public class Book {
+
+    String title;
+    String author;
+    String idISBN;
+
+    public Book(String title, String author, String idISBN) {
+        this.title = title;
+        this.author = author;
+        this.idISBN = idISBN;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Ironhacker");
+        Book b = new Book("allo", "salvatore" , "1234567890");
+        System.out.println("ISBN : " +b.idISBN  + "title :" +b.title + " - author :" + b.author);
     }
 }
